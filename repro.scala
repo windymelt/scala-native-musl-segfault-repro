15b6a8235dbb7bd6f006899f8f06050a8b89814c
windymelt <windymelt@3qe.us>
2026-06-15 16:12:45 +0900

    Update Scala to 3.3.8 and add -Yfuture-lazy-vals

--- repro.scala
+++ repro.scala
@@ -1,5 +1,7 @@
 //> using platform native
-//> using scala 3.3
+//> using scala "3.3.8"
+//> using nativeVersion "0.5.12"
+//> using options -Yfuture-lazy-vals -java-output-version:11
 //> using dep com.lihaoyi::os-lib::0.11.8
 
 object Repro {
